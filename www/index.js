da11b64d1833861c8fb1b243eaf6e3a608b35385
Ruben de Vries <ruben@rubensayshi.com>
2015-10-02 14:26:01 +0200

    cleanup a bit

--- www/index.js
+++ www/index.js
@@ -1,32 +1,37 @@
-///* // sha256 using createHash
-console.log(' == createHash == ');
 var createHash = require('create-hash');
-var hash = createHash('sha256');
-hash.update('synchronous write'); //optional encoding parameter
-
-var digest = hash.digest();
-
-console.log(digest.toString('hex') == "1e0a06bb7cb5399c5389c14ff182b748834f9aedbcfd5bb467e12bdde24a5370");
-
-console.log(digest.toString('hex'));
-// alert(digest.toString('hex'));
-
-console.log(digest.toString('base64'));
-// alert(digest.toString('base64'));
-//*/
-
-///* // sha256 using CryptoJS
-console.log(' == CryptoJS == ');
 var CryptoJS = require('crypto-js');
 
-var digest = CryptoJS.SHA256('synchronous write');
+var message = 'synchronous write';
 
-console.log(digest.toString() == "1e0a06bb7cb5399c5389c14ff182b748834f9aedbcfd5bb467e12bdde24a5370");
-// alert(digest.toString() == "1e0a06bb7cb5399c5389c14ff182b748834f9aedbcfd5bb467e12bdde24a5370");
+// sha256 using createHash
+true && (function() {
+    console.log(' == createHash == ');
+    var hash = createHash('sha256');
+    hash.update(message); //optional encoding parameter
 
-console.log(digest.toString());
-// alert(digest.toString());
+    var digest = hash.digest();
 
-console.log((new Buffer(digest.toString(), 'hex')).toString('base64'));
+    console.log(digest.toString('hex') == "1e0a06bb7cb5399c5389c14ff182b748834f9aedbcfd5bb467e12bdde24a5370");
+    // alert(digest.toString('hex') == "1e0a06bb7cb5399c5389c14ff182b748834f9aedbcfd5bb467e12bdde24a5370");
+
+    console.log(digest.toString('hex'));
+    // alert(digest.toString('hex'));
+
+    console.log(digest.toString('base64'));
+    // alert(digest.toString('base64'));
+})();
+
+// sha256 using CryptoJS
+true && (function() {
+    console.log(' == CryptoJS == ');
+    var digest = CryptoJS.SHA256(message);
+
+    console.log(digest.toString() == "1e0a06bb7cb5399c5389c14ff182b748834f9aedbcfd5bb467e12bdde24a5370");
+    // alert(digest.toString() == "1e0a06bb7cb5399c5389c14ff182b748834f9aedbcfd5bb467e12bdde24a5370");
+
+    console.log(digest.toString());
+    // alert(digest.toString());
+
+    console.log((new Buffer(digest.toString(), 'hex')).toString('base64'));
 // alert((new Buffer(digest.toString(), 'hex')).toString('base64'));
-//*/
+})();
